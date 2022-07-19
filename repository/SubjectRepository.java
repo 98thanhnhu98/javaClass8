ac3d2a88b0e8e409023a1f795dbf05f00c311a7c
98thanhnhu98 <97713475+98thanhnhu98@users.noreply.github.com>
2022-07-19 14:46:40 +0700

    Add files via upload

--- repository/SubjectRepository.java
+++ repository/SubjectRepository.java
@@ -2,12 +2,16 @@ package com.example.Example2.repository;
 
 import com.example.Example2.mode.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject,Long> {
     List<Subject> findAllByOrderByIdAsc();
     Subject findByName(String name);
     Subject findByNameAndSem(String name , long sem);
+    @Query("SELECT COUNT(s.name) FROM Subject s")
+    long getCountByName();
+
+    Long countSubjectsBySem(long sem);
 }
